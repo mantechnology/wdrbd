a4982c4ac3da2bdb223c23cedb797fdd06220798
jhkim <jhkim@mantech.co.kr>
2015-08-06 09:58:27 +0900

    drbd_transport class 초기화

--- drbd/drbd_transport.c
+++ drbd/drbd_transport.c
@@ -91,7 +91,8 @@ struct drbd_transport_class *drbd_get_transport_class(const char *name)
 #ifdef _WIN32_V9
 		// not support
 		// 드라이버 사용안함!
-		// 드라이버 로드 요청은 무시하고 직접 초기화, 최종 버전에서는 class 부분을 통째로 무시해도 될 듯
+		// 드라이버 로드 요청은 무시하고 직접 초기화, 이 작업으로 drbd_transport_ops 사용가능.
+		// 최종 버전에서는 class 부분을 통째로 무시해도 될 듯
 		extern int __init dtt_initialize(void);
 		dtt_initialize();
 #else
