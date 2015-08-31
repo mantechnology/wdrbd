fbca634705af25eba89b2da50e61181298943cb3
sekim <sekim@mantech.co.kr>
2015-08-31 10:03:11 +0900

    might_sleep 포팅 배제.

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -752,9 +752,7 @@ void __drbd_free_peer_req(struct drbd_peer_request *peer_req, int is_net)
 
 #ifdef _WIN32_V9
 	// V9에 새롭게 들어간 might_sleep. => lock을 갖지 않아서 sleep될 수 있음을 나타내는 코드... 리눅스의 스케줄링 관련 함수인데... 윈도우즈로의 포팅이 애매하다.
-#ifdef  _WIN32_CHECK
-	might_sleep(); //peer request 를 해제하는데... sleep 이 필요하나?... 
-#endif
+	//might_sleep(); //peer request 를 해제하는데... sleep 이 필요하나?... => might_sleep V9 포팅에서 배제. 메모리 해제 시 런타임 확인 필요. _WIN32_CHECK 
 	if (peer_req->flags & EE_HAS_DIGEST)
 		kfree(peer_req->digest);
 	drbd_free_pages(&peer_device->connection->transport, peer_req->pages, is_net);
@@ -770,19 +768,7 @@ void __drbd_free_peer_req(struct drbd_peer_request *peer_req, int is_net)
 	D_ASSERT(peer_device, drbd_interval_empty(&peer_req->i));
 	mempool_free(peer_req, drbd_ee_mempool);
 #endif
-	
 
-#if 0
-#ifdef _WIN32_TODO // __drbd_free_peer_req 인자 변경. V9 형식으로 변경 필요.
-	might_sleep();
-	if (peer_req->flags & EE_HAS_DIGEST)
-		kfree(peer_req->digest);
-	drbd_free_pages(&peer_device->connection->transport, peer_req->pages, is_net);
-	D_ASSERT(peer_device, atomic_read(&peer_req->pending_bios) == 0);
-	D_ASSERT(peer_device, drbd_interval_empty(&peer_req->i));
-	mempool_free(peer_req, drbd_ee_mempool);
-#endif
-#endif
 }
 #endif
 // <완료>
