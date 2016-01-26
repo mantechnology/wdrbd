bb6d20d8ab37f46f252711872bbf725f5a8cc7fb
sachoi <sachoi@mantech.co.kr>
2016-01-26 14:21:16 +0900

    DW-681 스크립트 및 install4j 수정

--- build/drbd/bin/drbdlog.cmd
+++ build/drbd/bin/drbdlog.cmd
@@ -1,31 +1,112 @@
 @echo off
-:: %1 - /F, /A 
-:: %2 - file name
-:: %3 - /T
-:: %4 - txt, evtx
+::
+:: USAGE: drbdlog [filename.etl] [/F <TXT|EVTX>][-h|-help]
+::
+
 setlocal
+
+set MAXSIZE=2
+set drbd_LogGuid={998bdf51-0349-4fbc-870c-d6130a955a5f}
 set PWD=%cd%
 set DIR=%WDRBD_PATH%\..\log
+set FILENAME=tracelog.etl
+set OUTPUTNAME=tracelog
+set FORMAT=txt
 cd "%DIR%"
 
-REM if !%1==! (
-REM	echo USAGE: drbdlog [convert] [all^|filename.etl] [/F] [TXT^|EVTX]
-REM	goto DEFAULT
-REM )
+
+if !%1==! (
+	goto CONVERT
+)
 
 
+set var=%1
+set var1=%var:"=%
+
+if /i "%var1%"=="-h" (
+	goto HELP
+)
+if /i "%var1%"=="-help" (
+	goto HELP
+)
+
+if /i "%var1%" == "/F" (
+	if not !%3==! (
+		goto ERROR
+	)
+	if !%2==! (
+		goto ERROR
+	)
+	set FORMAT=%2
+	goto CHECK_FORMAT
+
+) else (
+
+	FOR %%i IN ("%var1%") DO (
+		set OUTPUTNAME=%%~ni
+	)
+	set FILENAME=%var1%
+
+)
+if !%2==! (
+	goto CONVERT
+) else (
+	if not !%4==! (
+		goto ERROR
+	)
+	if !%3==! (
+		goto ERROR
+	) else (
+		set FORMAT=%3
+		goto CHECK_FORMAT
+	)
+)
+
+
+:CHECK_FORMAT
+if /i "%FORMAT%" == "txt" (
+	goto CONVERT
+) else if /i "%FORMAT%" == "evtx" (
+	goto CONVERT
+)else (	
+	goto ERROR
+)
+
+:CONVERT	
 logman stop wdrbdtrace -ets > nul 2>&1
+if %errorlevel% gtr 0 (
+	echo ERROR: Failed to start logger
+	goto ERROR
+)
 
+netsh trace convert "%FILENAME%" dump=%FORMAT% overwrite=yes > nul 2>&1
+if %errorlevel% gtr 0 (
+	echo ERROR: Cannot convert "%FILENAME%"
+	goto ERROR
+)
 
+start /B cmd /c "%DIR%\%OUTPUTNAME%.%FORMAT%" > nul 2>&1
 
-REM netsh trace convert %1 %2 %4 overwrite=yes > nul 2>&1
-REM goto LOGMAN
+if %errorlevel% gtr 0 (
+	echo ERROR: Cannot open "%DIR%\%OUTPUTNAME%.%FORMAT%"
+	goto ERROR
+)
 
-:DEFAULT
-netsh trace convert "%DIR%\tracelog.etl" overwrite=yes > nul 2>&1
+logman start trace "wdrbdtrace" -p %drbd_LogGuid% 0xffffffff 0xff -o "%DIR%\tracelog.etl" -mode 0x00001200 -ct system -ft 1 -max %MAXSIZE% -a -ets >nul 2>&1
+if %errorlevel% gtr 0 (
+	echo ERROR: Failed to start logger
+	goto ERROR
+)
+goto END
 
-:LOGMAN
-logman start trace "wdrbdtrace" -p drbd_LogGuid 0xffffffff 0xff -o "%DIR%\tracelog.etl" -mode 0x00001200 -ct system -ft 1 -max 1 -a -ets >nul 2>&1
-
+:HELP
+:ERROR
+echo USAGE: drbdlog [filename.etl] [/F ^<TXT^|EVTX^>][-h^|-help]
+echo EXAMPLES:
+echo 	drbdlog
+echo 	drbdlog /f txt
+echo 	drbdlog tracelog.etl
+echo 	drbdlog tracelog_20160125_180343.etl /f evtx
+:END
 cd "%PWD%"
 
--- build/drbd/bin/start_logging.bat
+++ build/drbd/bin/start_logging.bat
@@ -10,9 +10,21 @@ set S=%T:~6,2%
 
 set /a COUNT=0
 set MAXCOUNT=3
+set MAXSIZE=2
+set drbd_LogGuid={998bdf51-0349-4fbc-870c-d6130a955a5f}
 set DIR=%WDRBD_PATH%\..\log
 
-for /f %%i in ('dir "%DIR%\*etl.*" /b /O:D') do (
+
+set cmd="logman -ets|findstr wdrbdtrace"
+for /f %%i in ('%cmd%') do set X=%%i
+
+if not !%X%==! (
+	:: aready started
+	goto END
+)
+
+::file backup
+for /f %%i in ('dir "%DIR%\*.etl" /b /O:D') do (
 	@(set /a COUNT+=1 >nul)
 	if !COUNT! == 1 (
 		@(set DELFILE=%%i)
@@ -24,7 +36,7 @@ if %COUNT% GTR %MAXCOUNT% (
 	del /Q "%DIR%\%DELFILE%"
 )
 
-ren "%DIR%\tracelog.etl" "tracelog.etl.%D%_%H%%M%%S%" >nul 2>&1
-logman start trace "wdrbdtrace" -p drbd_LogGuid 0xffffffff 0xff -o "%DIR%\tracelog.etl" -mode 0x00001200 -ct system -ft 1 -max 1 -a -ets >nul 2>&1
-
+ren "%DIR%\tracelog.etl" "tracelog_%D%_%H%%M%%S%.etl" >nul 2>&1
+logman start trace "wdrbdtrace" -p %drbd_LogGuid% 0xffffffff 0xff -o "%DIR%\tracelog.etl" -mode 0x00001200 -ct system -ft 1 -max %MAXSIZE% -a -ets >nul 2>&1
+:END
 	
