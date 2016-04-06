07cb2f16c53ada415fbdce6892107f446618f241
sekim <sekim@mantech.co.kr>
2016-04-06 10:10:23 +0900

    DW-813 Merge 8.4/a3d3218 'drbd: change list_for_each_safe to while(list_first_entry_or_null)'
    Commit: e651c38db50c25f4a7573ae0bc6821fd531297f3 [e651c38]
    Parents: d27e2d19e3, a3d3218038
    Author: Lars Ellenberg <lars.ellenberg@linbit.com>
    Date: 2016년 4월 5일 화요일 오후 6:21:08
    Merge 8.4/a3d3218 'drbd: change list_for_each_safe to while(list_first_entry_or_null)'

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -2071,16 +2071,13 @@ static bool prepare_al_transaction_nonblock(struct drbd_device *device,
 					    struct list_head *pending,
 					    struct list_head *later)
 {
-	struct drbd_request *req, *tmp;
+	struct drbd_request *req;
 	int wake = 0;
 	int err;
 
 	spin_lock_irq(&device->al_lock);
-#ifdef _WIN32
-    list_for_each_entry_safe(struct drbd_request, req, tmp, incoming, tl_requests) {
-#else
-	list_for_each_entry_safe(req, tmp, incoming, tl_requests) {
-#endif
+
+	while ((req = list_first_entry_or_null(incoming, struct drbd_request, tl_requests))) {
 		err = drbd_al_begin_io_nonblock(device, &req->i);
 		if (err == -ENOBUFS)
 			break;
@@ -2099,12 +2096,9 @@ static bool prepare_al_transaction_nonblock(struct drbd_device *device,
 
 void send_and_submit_pending(struct drbd_device *device, struct list_head *pending)
 {
-	struct drbd_request *req, *tmp;
-#ifdef _WIN32
-    list_for_each_entry_safe(struct drbd_request, req, tmp, pending, tl_requests) {
-#else
-	list_for_each_entry_safe(req, tmp, pending, tl_requests) {
-#endif
+	struct drbd_request *req;
+
+	while ((req = list_first_entry_or_null(pending, struct drbd_request, tl_requests))) {
 		req->rq_state[0] |= RQ_IN_ACT_LOG;
 		req->in_actlog_jif = jiffies;
 		atomic_dec(&device->ap_actlog_cnt);
