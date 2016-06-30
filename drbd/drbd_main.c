d66848265bfdb67d7ba524af6d428582a3ddcb17
Hyunjun Lee <hjlee@mantech.co.kr>
2016-06-30 16:18:25 +0900

    DW-1000 1:2 star형 연결에서 실시간 복제를 받지 못한 노드로 resync가 되지 않는 문제 수정
    primary로 변경되어 복제 데이터가 발생할 때 uuid를 새로 만들지 않아, 실시간 복제 데이터를 받지 못한 노드와 데이터는 다르지만 같은 uuid를 가지고 있음.
    따라서 resync가 일어나지 않는다.
    
    rotate_current_into_bitmap 함수에서 md의 bitmap_index가 -1일 경우 스킵하는 부분을 제거하여, primary로 롤 변경 후 복제 데이터가 발생할 때 current uuid를 새로 만들 수 있도록 수정

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -5141,13 +5141,6 @@ static u64 rotate_current_into_bitmap(struct drbd_device *device, u64 weak_nodes
 	for (node_id = 0; node_id < DRBD_NODE_ID_MAX; node_id++) {
 		if (node_id == device->ldev->md.node_id)
 			continue;
-#ifdef _WIN32 // DW-837
-		if (peer_md[node_id].bitmap_index == -1) 
-		{
-			/* Skip bitmap indexes which are not assigned to a peer. */
-			continue;
-		}			
-#endif
 		bm_uuid = peer_md[node_id].bitmap_uuid;
 		if (bm_uuid)
 			continue;
