1c787769b6d2e27f32081dc732dd46ca9b8cee9e
Hyunjun Lee <hjlee@mantech.co.kr>
2016-07-15 19:33:18 +0900

    DW-1049 fix bsod when drbd attaches
    irregular memory corruption is detected when drbd is attaching and allocating memory.
    by using special pool, detected memory corrupter and put it out.

--- wdrbd9/sub.c
+++ wdrbd9/sub.c
@@ -175,10 +175,6 @@ mvolRemoveDevice(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
 			if (bd->bd_disk) {
 				blk_cleanup_queue(bd->bd_disk->queue);
 				bd->bd_disk->queue = NULL;
-				if (bd->bd_disk->private_data) {
-					struct drbd_backing_dev * pdbd = (struct drbd_backing_dev *)bd->bd_disk->private_data;
-					pdbd->md_bdev = NULL;
-				}
 				put_disk(bd->bd_disk);
 				bd->bd_disk = NULL;
 			}
