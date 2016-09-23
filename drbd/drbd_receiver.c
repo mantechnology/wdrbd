c4bd2e28be8c79193381172cf2ccfcfdd3d6587b
Hyunjun Lee <hjlee@mantech.co.kr>
2016-09-23 14:29:48 +0900

    DW-1176 restart drbd receiver when it can't re-try to connect(edited mistyped issue key).
    drbd tries to re-connect when it gets failed while connected and receiver is not running.
    above causes next connection be disconnected, and it tries to connect again, so peer node does.
    
    some kind of connecting options might be ignored since the node wanders between connection and disconnection.
    (discard-my-data is ignored on issue 'DW-1176')

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -1035,7 +1035,7 @@ retry:
 	conn_disconnect(connection);
 	schedule_timeout_interruptible(HZ);
 #ifdef _WIN32
-	// MODIFIED_BY_MANTECH DW-1076: retrying connection doesn't make sense while receiver's restarting, returning false lets drbd re-enters connection once receiver goes running.
+	// MODIFIED_BY_MANTECH DW-1176: retrying connection doesn't make sense while receiver's restarting, returning false lets drbd re-enters connection once receiver goes running.
 	if (get_t_state(&connection->receiver) == RESTARTING)
 	{
 		drbd_warn(connection, "could not retry connection since receiver is restarting\n");
