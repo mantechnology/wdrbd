a434b0c381301d2772fcd39045a48431a58e7510
sekim <sekim@mantech.co.kr>
2016-11-28 16:43:55 +0900

    DW-1291 provide last primary for disk detach & daisy chain reconnect case

--- drbd/drbd_state.c
+++ drbd/drbd_state.c
@@ -2854,19 +2854,19 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 				drbd_khelper(device, connection, "pri-on-incon-degr");
 
 #ifdef _WIN32 // DW-1291 provide LastPrimary Information for Local Primary
+			WDRBD_INFO("peer_role[NEW]:%d device->disk_state[OLD]:%d device->disk_state[NEW]:%d\n",peer_role[NEW],device->disk_state[OLD], device->disk_state[NEW]);
 			if( (role[OLD] == R_SECONDARY) && (role[NEW] == R_PRIMARY) ) {
 				if(get_ldev_if_state(device, D_NEGOTIATING)) {
 					drbd_md_set_flag (device, MDF_LAST_PRIMARY );
 					put_ldev(device);
 				}
+			} else if( (peer_role[NEW] == R_PRIMARY) 
+			|| ((role[NOW] == R_SECONDARY) && (resource->twopc_reply.primary_nodes != 0)) ) { // disk detach case || detach & reconnect daisy chain case
+				if(get_ldev_if_state(device, D_NEGOTIATING)) {
+					drbd_md_clear_flag (device, MDF_LAST_PRIMARY );
+					put_ldev(device);
+				}
 			} 
-			// this MDF_LAST_PRIMARY clear logic perform at process_twopc for daisy-chain connection case
-			//else if( peer_role[NEW] == R_PRIMARY ) { 
-			//	get_ldev(device);
-			//	drbd_md_clear_flag (device, MDF_LAST_PRIMARY );
-			//	put_ldev(device);
-			//}
-		
 #endif
 
 			if (susp_nod[NEW]) {
