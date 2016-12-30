6d4906a5f5ba8f40b231ef6e4ea77a672a421815
sekim <sekim@mantech.co.kr>
2016-12-30 13:07:47 +0900

    DW-1341 fixup send current state to all peer, for unstable trigger case

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -7167,9 +7167,14 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 #ifdef _WIN32_STABLE_SYNCSOURCE
 	// DW-1341 if UNSTABLE_TRIGGER bit is set , send uuids(unstable node triggering).
 	if(test_and_clear_bit(UNSTABLE_TRIGGER, &peer_device->flags)) {
+		struct drbd_device *device2 = peer_device->device;
+		struct drbd_peer_device *peer_device2;
+		u64 im;
 		drbd_send_uuids(peer_device, 0, 0);
-		for_each_peer_device(peer_device, device) {
-			drbd_send_current_state(peer_device);
+		for_each_peer_device_ref(peer_device2, im, device2) {
+			if (peer_device2->connection->cstate[NOW] == C_CONNECTED) {
+				drbd_send_current_state(peer_device2);
+			}
 		}
 	}
 #endif
