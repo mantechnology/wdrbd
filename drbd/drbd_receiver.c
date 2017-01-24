7f5ca5d40c5458f7ac88ba8d2be728d26ccb9c0c
won_sun <swon@mantech.co.kr>
2017-01-24 13:58:47 +0900

    DW-1382 [LINBIT] drbd: Allow to queue the same TID multiple times if it comes in on difference connections
    merge with previous commit

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -6337,7 +6337,8 @@ static int queue_twopc(struct drbd_connection *connection, struct twopc_reply *t
 	list_for_each_entry(q, &resource->queued_twopc, w.list) {
 #endif
 		if (q->reply.tid == twopc->tid &&
-		    q->reply.initiator_node_id == twopc->initiator_node_id)
+		    q->reply.initiator_node_id == twopc->initiator_node_id &&
+			q->connection == connection)
 			already_queued = true;
 	}
 	spin_unlock_irq(&resource->queued_twopc_lock);
