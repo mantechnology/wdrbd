3d9ca2d0b064dda4196d0586afe433ec29e43af9
Hyunjun Lee <hjlee@mantech.co.kr>
2017-02-09 14:10:43 +0900

    DW-1327 add drbdlock (un)installation, post build event and driver signing

--- build/drbd/bin/install.bat
+++ build/drbd/bin/install.bat
@@ -9,5 +9,6 @@ if not (%1)==() set DRIVER_PATH=%1
 cd %DRIVER_PATH%
 
 rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 0 .\drbd.inf
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 0 .\drbdlock.inf
 
 rem echo reboot...
