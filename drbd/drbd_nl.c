a59cd8d6b6fbe87848b50fef1cf836b6e4602ce0
Songah Choi <sachoi@mantech.co.kr>
2017-05-04 10:16:47 +0900

    DW-1323 add comment

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -1201,6 +1201,7 @@ retry:
                 for_each_peer_device_ref(peer_device, im, device) {
                     sector_t p_size = peer_device->max_size << 9;
                     sector_t l_size = get_targetdev_volsize(device->this_bdev->bd_disk->pDeviceExtension);
+					// DW-1323: abort initial full sync when target disk is smaller than source
                     if (drbd_current_uuid(device) == UUID_JUST_CREATED && (l_size > p_size))
                     {
                         rv = SS_TARGET_DISK_TOO_SMALL;
