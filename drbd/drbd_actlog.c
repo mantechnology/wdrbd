9d0beb81db23c7c45908586917e938717b1297a7
won_sun <swon@mantech.co.kr>
2017-10-31 10:48:45 +0900

    DW-1513-logic-bug-for-enr-x *Apply linbit patch drbd: prevent BUG_ON() in paranioa code [9ce832ce]

--- drbd/drbd_actlog.c
+++ drbd/drbd_actlog.c
@@ -639,7 +639,7 @@ bool put_actlog(struct drbd_device *device, unsigned int first, unsigned int las
 	spin_lock_irqsave(&device->al_lock, flags);
 	for (enr = first; enr <= last; enr++) {
 		extent = lc_find(device->act_log, enr);
-		if (!extent) {
+		if (!extent || extent->refcnt == 0) {
 			drbd_err(device, "al_complete_io() called on inactive extent %u\n", enr);
 			continue;
 		}
