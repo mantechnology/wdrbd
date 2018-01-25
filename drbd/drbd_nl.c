6b7444876de31f305180e6e24abf4760849cf42a
won_sun <swon@mantech.co.kr>
2018-01-25 15:58:10 +0900

    DW-1538-linbit-patch [LINBIT] drbd: Do not hold conf_update while doing conn_try_disconnect() (697e71c)

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -4773,11 +4773,12 @@ int adm_disconnect(struct sk_buff *skb, struct genl_info *info, bool destroy)
 
 	connection = adm_ctx.connection;
 	mutex_lock(&adm_ctx.resource->adm_mutex);
-	mutex_lock(&connection->resource->conf_update);
 	rv = conn_try_disconnect(connection, parms.force_disconnect);
-	if (rv >= SS_SUCCESS && destroy)
+	if (rv >= SS_SUCCESS && destroy) {
+		mutex_lock(&connection->resource->conf_update);
 		del_connection(connection);
-	mutex_unlock(&connection->resource->conf_update);
+		mutex_unlock(&connection->resource->conf_update);
+	}
 	if (rv < SS_SUCCESS)
 		retcode = rv;  /* FIXME: Type mismatch. */
 	else
@@ -6700,7 +6701,7 @@ int drbd_adm_down(struct sk_buff *skb, struct genl_info *info)
 {
 	struct drbd_config_context adm_ctx;
 	struct drbd_resource *resource;
-	struct drbd_connection *connection, *tmp;
+	struct drbd_connection *connection;
 	struct drbd_device *device;
 	int retcode; /* enum drbd_ret_code rsp. enum drbd_state_rv */
 #ifdef _WIN32
@@ -6708,6 +6709,7 @@ int drbd_adm_down(struct sk_buff *skb, struct genl_info *info)
 #else
 	unsigned i;
 #endif
+	u64 im;
 
 	retcode = drbd_adm_prepare(&adm_ctx, skb, info,
 			DRBD_ADM_NEED_RESOURCE | DRBD_ADM_IGNORE_VERSION);
@@ -6854,17 +6856,20 @@ int drbd_adm_down(struct sk_buff *skb, struct genl_info *info)
 		goto out;
 	}
 #endif
-	mutex_lock(&resource->conf_update);
-	for_each_connection_safe(connection, tmp, resource) {
+
+	for_each_connection_ref(connection, im, resource) {
 		retcode = conn_try_disconnect(connection, 0);
 		if (retcode >= SS_SUCCESS) {
+			mutex_lock(&resource->conf_update);
 			del_connection(connection);
+			mutex_unlock(&resource->conf_update);
 		} else {
 			drbd_msg_put_info(adm_ctx.reply_skb, "failed to disconnect");
-			goto unlock_out;
+			goto out;
 		}
 	}
 
+	mutex_lock(&resource->conf_update);
 	/* detach */
 #ifdef _WIN32
     idr_for_each_entry(struct drbd_device *, &resource->devices, device, i) {
