bbe0f2239931d6400ad4dd0700507561f2ee9a2a
Songah Choi <sachoi@mantech.co.kr>
2018-01-30 17:52:39 +0900

    DW-1569 [LINBIT-UTILS] drbdmeta compat: fixup fallback path for BLKZEROOUT for older kernels

--- drbd-utils/user/shared/drbdmeta.c
+++ drbd-utils/user/shared/drbdmeta.c
@@ -64,7 +64,12 @@
 
 #include "config.h"
 
-/* BLKZEROOUT, available on linux-3.6 and later. */
+/* BLKZEROOUT, available on linux-3.6 and later,
+* and maybe backported to distribution kernels,
+* even if they pretend to be older.
+* Yes, we encountered a number of systems that already had it in their
+* kernels, but not yet in the headers used to build userland stuff like this.
+*/
 #ifndef BLKZEROOUT
 #define BLKZEROOUT    _IO(0x12,127)
 #endif
@@ -1725,15 +1730,15 @@ static void zeroout_bitmap(struct format *cfg)
 	fprintf(stderr, "initializing bitmap (%u KB) to all zero\n",
 		(unsigned int)(bitmap_bytes >> 10));
 
-#ifdef _WIN32
-	errno = ENOTTY;
-#else // not support. execute the fallback code.
+#ifndef _WIN32 // not support. execute the fallback code.
 	err = ioctl(cfg->md_fd, BLKZEROOUT, &range);
 	if (!err)
 		return;
+	PERROR("ioctl(%s, BLKZEROOUT, [%llu, %llu]) failed", cfg->md_device_name,
+		(unsigned long long)range[0], (unsigned long long)range[1]);
 #endif
-
-	if (errno == ENOTTY) {
+	fprintf(stderr, "Using slow(er) fallback.\n");
+	{
 		/* need to sector-align this for O_DIRECT.
 		 * "sector" here means hard-sect size, which may be != 512.
 		 * Note that even though ALIGN does round up, for sector sizes
@@ -1760,10 +1765,7 @@ static void zeroout_bitmap(struct format *cfg)
 				percent_last_report = percent_done;
 			}
 		}
-		fprintf(stderr,"\r100%%\n");
-	} else {
-		PERROR("ioctl(%s) failed", cfg->md_device_name);
-		exit(10);
+		fprintf(stderr,"\r100%%\n");	
 	}
 }
 
