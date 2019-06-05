5f8fb4957acf224c8c537c586810f6c8ab5ac0dd
myyoo <myyoo_mantech@mantech.co.kr>
2019-06-05 09:05:03 +0900

    DW-1809 Modified to match the existing representation of events2.

--- drbd-utils/user/v9/drbdsetup.c
+++ drbd-utils/user/v9/drbdsetup.c
@@ -3914,7 +3914,7 @@ static int print_notifications(struct drbd_cmd *cm, struct genl_info *info, void
 		struct drbd_io_error_info io_error;
 		if (!drbd_io_error_info_from_attrs(&io_error, info)) {
 			printf(" disk:%s io:%s", drbd_disk_type_name(io_error.disk_type), drbd_io_type_name(io_error.io_type));
-			printf(" error_code:0x%08X sector:%llus size:%u", io_error.error_code, io_error.sector, io_error.size);
+			printf(" error-code:0x%08X sector:%llus size:%u", io_error.error_code, io_error.sector, io_error.size);
 		}
 		else {
 			dbg(1, "io_error info missing\n");
