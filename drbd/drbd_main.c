51a3a5bb03a3cdb3af52d08aa3969ca7e59e4201
swhan <swhan@mantech.co.kr>
2019-06-14 12:58:46 +0900

    DW-1829 BSOD when inactive_ee is free from drbd_destroy_connection() * inactive_ee must be free before peer_device.

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -4346,6 +4346,17 @@ void drbd_destroy_connection(struct kref *kref)
 		drbd_err(connection, "epoch_size:%d\n", atomic_read(&connection->current_epoch->epoch_size));
 	kfree(connection->current_epoch);
 
+	//DW-1829 : peer_req must be free before peer_device.
+	//DW-1696 : If the connecting object is destroyed, it also destroys the inactive_ee.
+	struct drbd_peer_request *peer_req, *t;
+	spin_lock(&resource->req_lock);
+	if (!list_empty(&connection->inactive_ee)) {
+		list_for_each_entry_safe(struct drbd_peer_request, peer_req, t, &connection->inactive_ee, w.list) {
+			list_del(&peer_req->w.list);
+			drbd_free_peer_req(peer_req);
+		}
+	}
+
 #ifdef _WIN32
     idr_for_each_entry(struct drbd_peer_device *, &connection->peer_devices, peer_device, vnr) {
 #else
@@ -4363,16 +4374,6 @@ void drbd_destroy_connection(struct kref *kref)
 		spin_lock_irq(&resource->req_lock);
 		idr_remove(&connection->peer_devices, vnr);
 		spin_unlock_irq(&resource->req_lock);
-	}
-
-	//DW-1696 : If the connecting object is destroyed, it also destroys the inactive_ee.
-	struct drbd_peer_request *peer_req, *t;
-	spin_lock(&resource->req_lock);
-	if (!list_empty(&connection->inactive_ee)) {
-		list_for_each_entry_safe(struct drbd_peer_request, peer_req, t, &connection->inactive_ee, w.list) {
-			list_del(&peer_req->w.list);
-			drbd_free_peer_req(peer_req);
-		}
 	}
 
 	spin_unlock(&resource->req_lock);
