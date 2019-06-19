0eff88fa33a0c46ff7561c7b746f9d5ff10f9620
swhan <swhan@mantech.co.kr>
2019-06-19 11:09:46 +0900

    DW-1799 it is deemed to be connected only when INITIAL_STATE_RECEIVED or UUID_JUST_CREATED.

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -2249,8 +2249,8 @@ static bool get_max_agreeable_size(struct drbd_device *device, uint64_t *max) __
 			 * double check on cstate ... */
 			if (peer_device->repl_state[NOW] >= L_ESTABLISHED ||
 				(peer_device->connection->cstate[NOW] >= C_CONNECTED && 
-				//DW-1799 It is deemed to be connected only when INITIAL_STATE_RECEIVED is set.
-				test_bit(INITIAL_STATE_RECEIVED, &peer_device->flags))) {
+				//DW-1799 It is deemed to be connected only when INITIAL_STATE_RECEIVED or UUID_JUST_CREATED.
+				(test_bit(INITIAL_STATE_RECEIVED, &peer_device->flags) || drbd_current_uuid(device) == UUID_JUST_CREATED))) {
 				/* If we still can see it, consider its last
 				 * known size, even if it may have meanwhile
 				 * detached from its disk.
