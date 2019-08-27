142330b6cfc7c393fa0d55433c24afe87a271b24
swhan <swhan@mantech.co.kr>
2019-08-27 15:19:20 +0900

    DW-1916 add mask information settings

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -2763,7 +2763,7 @@ static int split_recv_resync_read(struct drbd_peer_device *peer_device, struct d
 	ULONG_PTR offset;
 
 	int submit_count = 0;
-	ULONG_PTR bits;
+	ULONG_PTR bits, mask;
 
 	peer_req = read_in_block(peer_device, d);
 	if (!peer_req) {
@@ -2954,7 +2954,7 @@ static int split_recv_resync_read(struct drbd_peer_device *peer_device, struct d
 								atomic_add(split_peer_req->i.size << 9, &device->rs_sect_ev);
 
 								//DW-1916 if you receive resync data from peer_device other than syncsource, set out of sync for peer_device except for the current syncsource.
-								bits = DRBD_END_OF_BITMAP;
+								mask = bits = DRBD_END_OF_BITMAP;
 
 								if (!is_sync_target(peer_device)) {
 									struct drbd_peer_device* pd;
@@ -2962,12 +2962,13 @@ static int split_recv_resync_read(struct drbd_peer_device *peer_device, struct d
 										if (pd != peer_device && is_sync_target(pd)) {
 											drbd_info(peer_device, "resync with other nodes in progress\n");
 											clear_bit(pd->bitmap_index, &bits);
+											clear_bit(pd->bitmap_index, &mask); 
 											break;
 										}
 									}
 								}
 
-								drbd_set_sync(device, split_peer_req->i.sector, split_peer_req->i.size, bits, DRBD_END_OF_BITMAP);
+								drbd_set_sync(device, peer_req->i.sector, peer_req->i.size, bits, mask);
 
 								drbd_info(peer_device, "##unmarked bb(%llu), sector(%llu), offset(%u), count(%u)\n", marked_rl->bb, BM_BIT_TO_SECT(marked_rl->bb) + i, i, atomic_read(unmarked_count));
 
@@ -3026,7 +3027,7 @@ static int split_recv_resync_read(struct drbd_peer_device *peer_device, struct d
 		sync by the sync source with a P_PEERS_IN_SYNC packet soon. */
 
 		//DW-1916 if you receive resync data from peer_device other than syncsource, set out of sync for peer_device except for the current syncsource.
-		bits = DRBD_END_OF_BITMAP;
+		mask = bits = DRBD_END_OF_BITMAP;
 
 		if (!is_sync_target(peer_device)) {
 			struct drbd_peer_device* pd;
@@ -3034,12 +3035,13 @@ static int split_recv_resync_read(struct drbd_peer_device *peer_device, struct d
 				if (pd != peer_device && is_sync_target(pd)) {
 					drbd_info(peer_device, "resync with other nodes in progress\n");
 					clear_bit(pd->bitmap_index, &bits);
+					clear_bit(pd->bitmap_index, &mask);
 					break;
 				}
 			}
 		}
 
-		drbd_set_sync(device, peer_req->i.sector, peer_req->i.size, bits, DRBD_END_OF_BITMAP);
+		drbd_set_sync(device, peer_req->i.sector, peer_req->i.size, bits, mask);
 
 		if (drbd_submit_peer_request(device, peer_req, REQ_OP_WRITE, 0,
 			DRBD_FAULT_RS_WR) == 0)
